03de5d9aa5a68821413ffb76f946392160dc3445
Firecarr0t <dagelijksvloggamen@gmail.com>
2022-07-27 15:42:41 +0200

    notification and about me start [💄]
    Added side links/scroll down and a notification for copying discord tag. With about page start

--- app.js
+++ app.js
@@ -5,3 +5,43 @@ function dropDown() {
     navbarLinks.classList.toggle('open')
     toggleButton.classList.toggle('open')
 }
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    if (offset > 900)
+    {
+        document.getElementById('nav').style.background = "var(--background-color2)"
+        document.getElementById('nav').style.boxShadow = "none"
+    }
+    else if (offset < 900)
+    {
+        document.getElementById('nav').style.background = "var(--background-color1)"
+        document.getElementById('nav').style.boxShadow = "10px 14px 30px var(--background-color1-light);"
+    }
+})
+
+function githubRedirect(){
+    location.href = 'https://github.com/ivarvandenBosch'
+}
+
+let notif = document.getElementById('notif')
+
+function discordCopy(){
+  var copyText = document.getElementById("text");
+
+  copyText.select();
+
+  //Mobile 
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+  notif.classList.add('animation')
+  setTimeout(function(){ 
+    notif.classList.remove('animation')
+   }, 2000)
+}
+
+function scrollDown()
+{
+    window.scrollTo(0, 4000);
+}
